ebc7095ac255e7ce458b03be0fd79300c28fa22b
innovativeGem <innovativeGem@gmail.com>
2021-05-08 08:09:39 +1000

    Added ExpenseItem component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <div>Jan 01 2021</div>
+            <div>
+                <h2>Car Insurance</h2>
+                <div>$294.57</div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
