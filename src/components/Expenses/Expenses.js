361671e7e9e14a2bc4e38981254dd255c9f7851c
innovativeGem <innovativeGem@gmail.com>
2021-05-18 07:19:33 +1000

    improved new expense functionality

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
 
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 import './Expenses.css';
 
-function Expenses(props) {
+const Expenses = (props) => {
 
-  const [expenseYear, setExpenseYear]= useState('2021');
+  const [expenseYear, setExpenseYear] = useState('2021');
 
   const expenseYearHandler = (year) => {
     setExpenseYear(year);
   }
+
+  const filterExpenses = props.expenses.filter(expense => expense.date.getFullYear().toString() === expenseYear);
 
   return (
     <div className="expenses">
@@ -20,26 +22,7 @@ function Expenses(props) {
         year={expenseYear}
       />
       <Card>
-        <ExpenseItem
-          title={props.expenses[0].title}
-          amount={props.expenses[0].amount}
-          date={props.expenses[0].date}
-        />
-        <ExpenseItem
-          title={props.expenses[1].title}
-          amount={props.expenses[1].amount}
-          date={props.expenses[1].date}
-        />
-        <ExpenseItem
-          title={props.expenses[2].title}
-          amount={props.expenses[2].amount}
-          date={props.expenses[2].date}
-        />
-        <ExpenseItem
-          title={props.expenses[3].title}
-          amount={props.expenses[3].amount}
-          date={props.expenses[3].date}
-        />
+        <ExpensesList expenses={filterExpenses} />
       </Card>
     </div>
   );
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+
+    const [showExpenseForm, setExpenseForm] = useState(false);
 
     const saveExpenseDataHandler = (expenseData) => {
         const newExpenseData = {
@@ -11,10 +13,21 @@ const NewExpense = (props) => {
             id: Math.random().toFixed(3).toString()
         }
         props.onAddNewExpense(newExpenseData);
+        setExpenseForm(!showExpenseForm);
+    }
+
+    const toggleExpenseFormHandler = () => {
+        setExpenseForm(!showExpenseForm);
+    }
+
+    let newExpense = <button onClick={toggleExpenseFormHandler}>Add New Expense</button>
+
+    if (showExpenseForm) {
+        newExpense = <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancelExpenseData={toggleExpenseFormHandler} />
     }
 
     return <div className="new-expense">
-        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+        {newExpense}
     </div>
 }
 
